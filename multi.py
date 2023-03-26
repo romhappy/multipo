7fe78353478f2157446ea9417f889de9eea3bfe4
romhappy <romainbesin21@gmail.com>
2023-03-26 22:08:33 +0200

    first commit

---
+++ multi.py
@@ -0,0 +1,80 @@
+import random
+import time
+
+
+number_1=random.randint(0,10)
+number_2=random.randint(0,10)
+result_1 = number_1*number_2
+print(str(number_1)+" X "+str(number_2))
+
+time.sleep(3)   
+
+number_3=random.randint(0,10)
+number_4=random.randint(0,10)
+result_2 = number_3*number_4
+print(str(number_2)+" X "+str(number_3))
+
+time.sleep(3)  
+
+number_5=random.randint(0,10)
+number_6=random.randint(0,10)
+result_3 = number_5*number_6
+print(str(number_5)+" X "+str(number_6))
+
+time.sleep(3)  
+
+number_7=random.randint(0,10)
+number_8=random.randint(0,10)
+result_4 = number_7*number_8
+print(str(number_7)+" X "+str(number_8))
+
+time.sleep(3)  
+
+number_9=random.randint(0,10)
+number_10=random.randint(0,10)
+result_5 = number_9*number_10
+print(str(number_9)+" X "+str(number_10))
+
+time.sleep(3)  
+
+number_11=random.randint(0,10)
+number_12=random.randint(0,10)
+result_6 = number_11*number_12
+print(str(number_11)+" X "+str(number_12))
+
+time.sleep(3)  
+
+number_13=random.randint(0,10)
+number_14=random.randint(0,10)
+result_7 = number_13*number_14
+print(str(number_13)+" X "+str(number_14))
+
+time.sleep(3)  
+
+number_15=random.randint(0,10)
+number_16=random.randint(0,10)
+result_8 = number_15*number_16
+print(str(number_15)+" X "+str(number_16))
+
+time.sleep(3)  
+
+number_17=random.randint(0,10)
+number_18=random.randint(0,10)
+result_9 = number_17*number_18
+print(str(number_17)+" X "+str(number_18))
+
+time.sleep(3)  
+
+number_19=random.randint(0,10)
+number_20=random.randint(0,10)
+result_10 = number_19*number_20
+print(str(number_19)+" X "+str(number_20))
+
+time.sleep(3)  
+
+print("Et voici les résultats!")
+
+time.sleep(3)  
+
+print(result_1,result_2,result_3,result_4,result_5,result_6,result_7,result_8,result_9,result_10)
+
